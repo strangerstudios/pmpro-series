2363eae6aea6b859d41513e6d0eb6795e23f95c6
Thomas Sjolshagen <Thomas@eighty20results.com>
2014-05-05 22:01:32 -0400

    Updated comments to reflect new "Sequence(s)" verbiage

--- pmpro-series.php
+++ pmpro-series.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: PMPro Series
+Plugin Name: PMPro Sequence
 Plugin URI: http://www.paidmembershipspro.com/pmpro-series/
 Description: Offer serialized (drip feed) content to your PMPro members.
 Version: .2.4
@@ -11,16 +11,16 @@ Author URI: http://www.strangerstudios.com
 /*
 	The Story
 	
-	1. There will be a new "Series" tab in the Memberships menu of the WP dashboard.
-	2. Admins can create a new "Series".
-	3. Admins can add a page or post to a series along with a # of days after signup.
-	4. Admins can add a series to a membership level.
+	1. There will be a new "Sequences" tab in the Memberships menu of the WP dashboard.
+	2. Admins can create a new "Sequence".
+	3. Admins can add a page or post to a series along with a # of days after signup (or a specific date)
+	4. Admins can add a sequence to a membership level.
 	5. Admins can adjust the email template via an added page to their active theme.
 	
 	Then...
 	
-	1. User signs up for a membership level that gives him access to Series A.
-	2. User gets access to any "0 days after" series content.
+	1. User signs up for a membership level that gives him access to Sequence A.
+	2. User gets access to any "0 days after" (or on a specific date) series content.
 	3. Each day a script checks if a user should gain access to any new content, if so:
 	- User is given access to the content.
 	- An email is sent to the user letting them know that content is available.	
@@ -28,20 +28,20 @@ Author URI: http://www.strangerstudios.com
 	Checking for access:
 	* Is a membership level required?
 	* If so, does the user have one of those levels?
-	* Is the user's level "assigned" to a series?
+	* Is the user's level "assigned" to a sequence?
 	* If so, does the user have access to that content yet? (count days)
 	* If not, then the user will have access. (e.g. Pro members get access to everything right away.)
 	
 	Checking to send emails: (planned feature)
-	* For all members with series levels.
+	* For all members with sequence levels.
 	* What day of the membership is it?
-	* For all series.
+	* For all sequences.
 	* Get content.
 	* Send content for this day.
 	* Email update.
 	
 	Data Structure
-	* Series is a CPT
+	* Sequence is a CPT
 	* Use wp_pmpro_memberships_pages to link to membership levels
 	* wp_pmpro_series_content (series_id, post_id, day) stored in post meta
 */
